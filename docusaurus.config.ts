f7cb77ad274e25abbdb10dc505d31d370dc522c2
Nicolas Gonzalez <ngonzalez@operametrix.fr>
2025-09-25 19:01:49 +0200

    Change repo and org name

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -23,8 +23,9 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'operametrix', // Usually your GitHub org/user name.
+  projectName: 'ignition-modules-docs', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
 
