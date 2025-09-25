991d7624a015161e18f3beb2b22c3a74c65fa5f2
Nicolas Gonzalez <ngonzalez@operametrix.fr>
2025-09-25 21:14:58 +0200

    Change baseurl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -16,10 +16,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://operametrix.github.io',
+  url: 'https://operametrix.github.io/ignition-modules-docs',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/ignition-modules-docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
